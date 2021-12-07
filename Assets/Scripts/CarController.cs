fdc96a9731a2ac302d862846755efaae7eb1e7e1
artlevandoski <artlevandoski@hotmail.com>
2021-12-07 15:57:19 -0500

    Added Car Controller Script

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public float acceleration;
+   public float turnSpeed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public Transform carModel;
+   private Vector3 startModelOffset;
+
+   public float groundCheckRate;
+   private float lastGroundCheckTime;
+
+   private float curYrot;
+
+   private bool accelerateInput;
+   private float turnInput;
+
+   public Rigidbody rig;
+
+   public void OnAccelerateInput (InputAction.CallbackContext context)
+   {
+        if(context.phase == InputActionPhase.Performed)
+            accelerateInput = true;
+        else 
+            accelerateInput = false;
+   }
+
+   public void OnTurnInput (InputAction.CallbackContext context)
+
+        {
+            turnInput = context.ReadValue<float>();
+        }
+
+
+
+
+
 }
