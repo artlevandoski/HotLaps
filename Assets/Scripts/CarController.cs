aa018031b9c0f22384c348ee6e5f239b79048cbd
artlevandoski <artlevandoski@hotmail.com>
2021-12-07 18:07:43 -0500

    added Car Controller Script for Acceleration and Turning

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -21,7 +21,30 @@ public class CarController : MonoBehaviour
 
    public Rigidbody rig;
 
-   public void OnAccelerateInput (InputAction.CallbackContext context)
+   void Start ()
+   {
+       startModelOffset = carModel.transform.localPosition; //local position of the car model
+   }
+
+   //Update runs at the games fps
+   void Update ()
+   {
+       curYrot += turnInput * turnSpeed * Time.deltaTime; //uses Y axis to turn the car
+
+       carModel.position = transform.position + startModelOffset; //sets car model position
+       carModel.eulerAngles = new Vector3(0, curYrot, 0); //sets rotation to not rotate with the car
+   }
+
+   //Fixed Update always runs at 60 times per second, this is good for physics calculations which unity uses
+   void FixedUpdate ()
+   {
+       if(accelerateInput == true)
+       {
+           rig.AddForce(carModel.forward * acceleration, ForceMode.Acceleration); //begins pushing the car model forward ignoring mass
+       }
+   }
+
+   public void OnAccelerateInput (InputAction.CallbackContext context) //used to determine if accelerate button is pushed down or not
    {
         if(context.phase == InputActionPhase.Performed)
             accelerateInput = true;
@@ -29,7 +52,7 @@ public class CarController : MonoBehaviour
             accelerateInput = false;
    }
 
-   public void OnTurnInput (InputAction.CallbackContext context)
+   public void OnTurnInput (InputAction.CallbackContext context) //used to determine the variable of the turning negative or postive
 
         {
             turnInput = context.ReadValue<float>();
