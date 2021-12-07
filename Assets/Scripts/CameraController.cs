1d2210b152ba2c718beae1a86af2623a936efe1a
artlevandoski <artlevandoski@hotmail.com>
2021-12-07 18:22:41 -0500

    Camera Controller Implemented

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+    public float followSpeed;
+    public float rotateSpeed;
+    void Update()
+    {
+        // syncing the rotation and position of the camera and the car over time
+        transform.position = Vector3.Lerp(transform.position,target.position, followSpeed * Time.deltaTime); //Lerp stands for Linear interpolation 
+        transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, rotateSpeed * Time.deltaTime);
+    }
+}
