ca60f5eef87e3abb68ec1559ab93f35e6ff53de9
artlevandoski <artlevandoski@hotmail.com>
2021-12-11 16:41:04 -0500

    Implemented Player Position UI

---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerUI : MonoBehaviour
+{
+    public TextMeshProUGUI carPositionText;
+    public CarController car;
+
+    void Update()
+        {
+            carPositionText.text = car.racePosition.ToString() + " / " + GameManager.instance.cars.Count.ToString();
+        }
+
+
+}
